18f53625583022ed8b1a1fa2c31446b2a18aa656
Melike Görgülü <melikegorgulu.119@gmail.com>
2022-08-17 17:12:51 +0300

    strenght bar added

--- main.js
+++ main.js
@@ -11,29 +11,40 @@ const symbolsInput = document.querySelector("#symbols");
 const uppersInput = document.querySelector("#uppers");
 const lowersInput = document.querySelector("#lowers");
 const alert = document.querySelector(".alert");
+const strenghtLine = document.querySelector(".strenght-line");
 
 const generator = () => {
   let chars = "";
   let key = "";
+  let bar = 0;
   if (numbersInput.checked) {
     chars += numbers;
+    bar += 0.25;
   }
   if (symbolsInput.checked) {
     chars += symbols;
+    bar += 0.25;
   }
   if (uppersInput.checked) {
     chars += uppers;
+    bar += 0.25;
   }
   if (lowersInput.checked) {
     chars += lowers;
+    bar += 0.25;
   }
   if (chars == "") {
-    showAlert("select at least one checkbox!");
+    showAlert("Select at least one checkbox!");
   }
-  for (let i = 1; i <= lenghtInput.value; i++) {
-    key += chars.charAt(Math.floor(Math.random() * chars.length));
+  if (lenghtInput.value > 5 && lenghtInput.value < 25) {
+    for (let i = 1; i <= lenghtInput.value; i++) {
+      key += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    pw.innerHTML = key;
+    showBarLine(bar);
+  } else {
+    showAlert("Character lenght must be in range of 5 - 25!");
   }
-  pw.innerHTML = key;
 };
 
 function copyKey() {
@@ -52,4 +63,30 @@ function showAlert(message) {
   const myTimeout = setTimeout(() => {
     alert.style.bottom = "-50px";
   }, 1000);
+}
+
+function showBarLine(bar) {
+  if (lenghtInput.value >= 8) {
+    if (bar == 0.25) {
+      strenghtLine.style.width = "25%";
+      strenghtLine.style.backgroundColor = "#C21010";
+    } else if (bar == 0.5) {
+      strenghtLine.style.width = "40%";
+      strenghtLine.style.backgroundColor = "#ffbb29";
+    } else if (bar == 0.75) {
+      strenghtLine.style.width = "60%";
+      strenghtLine.style.backgroundColor = "#FFC23C";
+    } else {
+      strenghtLine.style.width = "100%";
+      strenghtLine.style.backgroundColor = "#019267";
+    }
+  } else {
+    if (bar >= 0.75) {
+      strenghtLine.style.width = "50%";
+      strenghtLine.style.backgroundColor = "#FFC23C";
+    } else {
+      strenghtLine.style.width = "25%";
+      strenghtLine.style.backgroundColor = "#C21010";
+    }
+  }
 }
